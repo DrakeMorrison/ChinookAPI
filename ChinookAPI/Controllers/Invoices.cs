9e5502b17fa862240cca31c670a7545c8bc2437f
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-10-22 20:45:15 -0500

    api controller created

---
+++ ChinookAPI/Controllers/Invoices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChinookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetByAgentId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
